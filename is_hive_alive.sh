4e218b8b35c41dec85dbc0daabc1dd3aefc88e8f
manojitp <manojitp@yahoo.com>
2015-12-24 09:46:33 -0800

    Update is_hive_alive.sh
    Added comments

--- is_hive_alive.sh
+++ is_hive_alive.sh
@@ -1,44 +1,48 @@
-#/bin/bash
+#!/bin/bash
+
+#displays parameter expected by the utility on screen 
 usage()
 {
 cat << EOF
 usage: $0 <threshold in seconds>
 EOF
 }
-timeout() {
 
+#function to invoke a shell command and wait for a specified time for it to complete
+timeout() {
+    #parameter 1 is timeout in seconds
     time=$1
+    #parameter 2 is the the command to invoke 
     command="/bin/sh -c \"$2\""
+    #spawn a shell using expect, set its environment before invoking the command, exit 1 if the command is still running
     expect -c "set echo \"-noecho\"; set timeout $time; spawn -noecho $command; expect timeout { exit 1 } eof { exit 0 }"    
     if [ $? = 1 ] ; then
-        echo "Timeout after ${time} seconds"
 	return 1;
     fi
     return 0;
 }
+
+#main takes one parameter, it can be modified to take additional parameters like the command to invoke and the password
 main() {
+    #print how to invoke the utility if not invoked with one parameter
     if [ $# != 1 ] ; then
         usage ; exit 1
     fi
     TIMEOUT_IN_SECONDS=$1
-    PWD="cA?2KjOg01dS"
+    PWD="password"
     echo "Timeout: $TIMEOUT_IN_SECONDS"
-    timeout $TIMEOUT_IN_SECONDS "hive -f /apps/scripts/use_warehouse.hive"
+
+    #hive can be invoked from the command line to use a file containing commands 
+    timeout $TIMEOUT_IN_SECONDS "hive -f /apps/scripts/file_containing_command.hive"
     if [ $? = 1 ] ; then
+        #prepare to send an email alert to admin
         MAIL_FILE="/tmp/mail_hive_down.txt"
         SUBJECT="ALERT from `hostname` - Connection to Hive metastore timed out."
-        CC_EMAIL="manprasa@adobe.com"
-        #EMAIL="noc@adobe.com"
-        EMAIL="IT-Hadoop-Support@adobe.com"
-        #STATUS=`curl -u admin:$PWD 'http://sj1dra010.corp.adobe.com:7180/api/v1/clusters/SJ-HDP01/services/hive2' | python -mjson.tool | grep serviceState | sed 's/.*serviceState\"\: \"\(.*\)\"\,.*/\1/'`
-        #FORMATTED_LINE="Current status of the service as per CM API:[${STATUS}]"
-        #echo $FORMATTED_LINE
-        #echo $FORMATTED_LINE > $MAIL_FILE
-        #FORMATTED_LINE="Hive metastore is unreachable. Waited $TIMEOUT_IN_SECONDS seconds. The service should have been automtically restarted. Please verify ASAP. "
+        EMAIL="administrators_email_address"
         FORMATTED_LINE="Hive metastore is unreachable. Waited $TIMEOUT_IN_SECONDS seconds. Please verify ASAP. "
-        echo $FORMATTED_LINE
         echo $FORMATTED_LINE > $MAIL_FILE
 
+        #send email
         mail -E -c "$CC_EMAIL" -s "$SUBJECT" "$EMAIL" < "$MAIL_FILE"
     fi
 }
